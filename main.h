a0e1ae40bdc8e2fd0990a2f277c0b060bc0160a5
Tadas Nevinskas <tadasnevinskas@gmail.com>
2020-07-18 11:24:54 +0100

    main.h: set TURN_PWM_RANGE to JS_AXIS_MAX
    Joystick axis maximum value is JS_AXIS_MAX, but it can go to negative
    numbers. By turning analog to left, the farest value is -32767, by
    turning right 32767.
    
    PWM range should be set to this maximum.

--- main.h
+++ main.h
@@ -11,7 +11,7 @@
 
 #define TURN_PIN		RPI_V2_GPIO_P1_33
 #define TURN_PWM_CH		1
-#define TURN_PWM_RANGE		65535
+#define TURN_PWM_RANGE		JS_AXIS_MAX
 
 #define IN0			RPI_V2_GPIO_P1_11
 #define IN1			RPI_V2_GPIO_P1_13
